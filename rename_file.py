23d88350dc9568f28607d559ce7685718f7a8423
Paul <twinencrypt@gmail.com>
2020-11-18 16:48:22 +0700

    Added IntVar for events at radiobuttons

--- rename_file.py
+++ rename_file.py
@@ -1,9 +1,10 @@
-from tkinter import Tk, filedialog, Text, Button, Label, LabelFrame
+from tkinter import Tk, filedialog, Text, Button, Label, Radiobutton, Toplevel, IntVar
 from tkinter.messagebox import *
 from rename_function import RenameFunction
 
 # Создаем объект класса RenameFunction
 rf = RenameFunction()
+bg_root_color = "#81cc96"
 
 
 def read_files_from_directory(event):
@@ -13,19 +14,49 @@ def read_files_from_directory(event):
     rf.read_files_from_folder()
 
 
-def press_button_replace_char(event):
-    chars = char_field.get(1.0, "end")
-    rf.replace_char(chars)
+def press_button_replace():
+    top_replace = Toplevel()
+    top_replace.title("Заменить")
+    top_replace.geometry("250x250")
+    top_replace.resizable(width=False, height=False)
+    top_replace.config(bg="lightblue")
+    bg_top_color = "white"
+    submenu_replace_label1 = Label(top_replace, text="Заменить с:", height=1, bg="lightblue")
+    submenu_replace_text1 = Text(top_replace, height=1, bg=bg_top_color, fg='black', takefocus=True)
+    submenu_replace_label2 = Label(top_replace, text="Заменить на:", width=15, height=1, bg="lightblue")
+    submenu_replace_text2 = Text(top_replace, height=1, bg=bg_top_color, fg='black', takefocus=True)
+    submenu_replace_label1.pack()
+    submenu_replace_text1.pack()
+    submenu_replace_label2.pack()
+    submenu_replace_text2.pack()
+    btn = Button(top_replace, text='Заменить')
+    btn['command'] = lambda: rf.replace_char(submenu_replace_text1.get(1.0, "end"),
+                                             submenu_replace_text2.get(1.0, "end"))
+    btn.pack(side="bottom")
 
 
-def press_button_delete_char(event):
-    chars = char_field.get(1.0, "end")
-    rf.delete_char(chars)
+def press_button_delete():
+    top_del = Toplevel()
+    top_del.title("Заменить")
+    top_del.geometry("250x250")
+    top_del.resizable(width=False, height=False)
+    top_del.config(bg="lightblue")
+    bg_top_color = "white"
+    submenu_delete_label1 = Label(top_del, text="Введите количество символов для удаления с начала строки:", height=1, bg="lightblue")
+    submenu_delete_text1 = Text(top_del, height=1, bg=bg_top_color, fg='black')
+    submenu_delete_label1.pack()
+    submenu_delete_text1.pack()
+    btn2 = Button(top_del, text='Удалить')
+    if click.get():
+        print('begin', click.get())
 
-
-def press_button_add_char(event):
-    chars = char_field.get(1.0, "end")
-    rf.add_char(chars)
+    # if r_but_del_begin_str.focus:
+    #     print("begin")
+    #     # btn2['command'] = lambda: rf.delete_char(submenu_delete_text1.get(1.0, "end"), True)
+    # if r_but_del_end_str.focus:
+    #     print("end")
+    #     # btn2['command'] = lambda: rf.delete_char(submenu_delete_text1.get(1.0, "end"), False)
+    btn2.pack(side="bottom")
 
 
 # Основное окно
@@ -35,22 +66,14 @@ root.title("Rename Files")
 root.geometry("600x400")
 root.resizable(width=False, height=False)
 
+
 # Label для директории
-dir_label = Label(root, text="Открыть папку:", width=15, height=1, bg="#81cc96")  # , bg="#81cc96"
+dir_label = Label(root, text="Открыть папку:", width=15, height=1, bg=bg_root_color)  # , bg="#81cc96"
 dir_label.place(x=250, y=50)
 
 # Поле для ввода и вывода пути к директории
 dir_field = Text(root, width=35, height=1, bg="#404040", fg='white')
 dir_field.place(x=160, y=70)
-
-
-# Label для ввода символов
-char_label = Label(root, text="Введите символы:", width=15, height=1, bg="#81cc96")  # , bg="#81cc96"
-char_label.place(x=250, y=140)
-
-# Поле для ввода символов
-char_field = Text(root, width=15, height=1, bg="#404040", fg='white')
-char_field.place(x=240, y=160)
 
 
 # Кнопка для выбора директории с файлами
@@ -59,22 +82,37 @@ btn_choice_dir.place(x=444, y=70)
 btn_choice_dir.bind("<Button-1>", read_files_from_directory)
 
 
-# Кнопка для выполения метода replace_char
-btn_replace = Button(root, text='Заменить', height=1)
-btn_replace.place(x=100, y=250)
-btn_replace.bind("<Button-1>", press_button_replace_char)
+# --------------------NEW CODE-------------------------------
+r_but_replace = Radiobutton(root, text='Заменить', bg=bg_root_color, value=0)
+r_but_replace['command'] = press_button_replace
+# r_but_replace.config(state="disable")
+r_but_replace.place(x=90, y=120)
 
 
-# Кнопка для выполения метода delete_char
-btn_del = Button(root, text='Удалить', height=1)
-btn_del.place(x=255, y=250)
-btn_del.bind("<Button-1>", press_button_delete_char)
+click = IntVar()
+click.set(0)
 
+r_but_del_begin_str = Radiobutton(root, text='Удалить с начала строки', bg=bg_root_color, value=1, variable=click)
+r_but_del_begin_str['command'] = press_button_delete
 
-# Кнопка для выполения метода delete_char
-btn_add = Button(root, text='Добавить', height=1)
-btn_add.place(x=400, y=250)
-btn_add.bind("<Button-1>", press_button_add_char)
+# r_but_del_begin_str['variable'] = click
+# r_but_replace.config(state="disable")
+r_but_del_begin_str.place(x=190, y=105)
+
+r_but_del_end_str = Radiobutton(root, text='Удалить с конца строки', bg=bg_root_color, value=2, variable=click)
+r_but_del_end_str['command'] = press_button_delete
+
+# r_but_del_begin_str['variable'] = click2
+# r_but_replace.config(state="disable")
+r_but_del_end_str.place(x=190, y=135)
+
+r_but_add_begin_str = Radiobutton(root, text='Добавить с начала строки', bg=bg_root_color, value=3)
+# r_but_replace.config(state="disable")
+r_but_add_begin_str.place(x=380, y=105)
+
+r_but_add_end_str = Radiobutton(root, text='Добавить в конец строки', bg=bg_root_color, value=4)
+# r_but_replace.config(state="disable")
+r_but_add_end_str.place(x=380, y=135)
 
 
 # Главный цикл программы
--- rename_function.py
+++ rename_function.py
@@ -12,35 +12,32 @@ class RenameFunction:
         self.list_of_files = os.listdir(self.path_files)
         os.chdir(self.path_files)
 
-    def replace_char(self, input_str):
+    def replace_char(self, replace_src, replace_dist):
         """Производит замену с replace_src на replace_dist каждого файла списка list_of_files"""
-        replace_src = (input_str.split(',')[0]).strip()
-        replace_dist = (input_str.split(',')[1]).strip()
-        # os.chdir(self.path_files)
+        replace_src = replace_src.strip()
+        replace_dist = replace_dist.strip()
         self.read_files_from_folder()
         for name in self.list_of_files:
             os.rename(name, name.replace(replace_src, replace_dist))
 
-    def delete_char(self, input_str):
+    def delete_char(self, num_of_chars, begin):
         """Удаляет num_of_chars символов сначала строки, если флаг begin=True
          и с конца строки, если begin=False"""
-        num_of_chars = int((input_str.split(',')[0]).strip())
-        begin = int((input_str.split(',')[1]).strip())
-        # os.chdir(self.path_files)
+        # num_of_chars = int((input_str.split(',')[0]).strip())
+        # begin = int((input_str.split(',')[1]).strip())
         self.read_files_from_folder()
-        if begin == 1:
+        if begin:
             for name in self.list_of_files:
-                os.rename(name, name[num_of_chars:])
+                os.rename(name, name[int(num_of_chars):])
         else:
             for name in self.list_of_files:
                 index = name.rfind('.')
-                os.rename(name, name[:index - num_of_chars] + name[index:])
+                os.rename(name, name[:index - int(num_of_chars)] + name[index:])
 
     def add_char(self, input_str):
         """Добавялет chars символов в начало строки, если begin=True, если нет, то в конец"""
         chars = (input_str.split(',')[0]).strip()
         begin = int((input_str.split(',')[1]).strip())
-        # os.chdir(self.path_files)
         self.read_files_from_folder()
         # print(os.getcwd())
         # print("chars = {0}\t begin = {1}".format(chars, begin))
